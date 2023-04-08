73a4b0ade3a36624e704a95ed960e3d25ce5f804
Batu Özçamlık <bozcamlik@gmail.com>
2023-04-08 17:06:22 +0300

    Kapı Kodu Eklendi

---
+++ Assets/Script/DoorMng.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class DoorMng : MonoBehaviour
+{
+    private Animator anim;
+    public bool isLocked = true;
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+    private void Start()
+    {
+        //Invoke("Open", 2f);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+     
+        if (other.gameObject.tag=="Player")
+        {
+           
+            if(isLocked==false)
+            {
+                Open();
+            }
+          
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+      
+ 
+        if (other.gameObject.tag == "Player")
+        {
+           
+            Close();
+        }
+    }
+
+    public void Open()
+    {
+        anim.SetBool("character_nearby", true);
+    }
+    public void Close()
+    {
+        anim.SetBool("character_nearby", false);
+    }
+}
